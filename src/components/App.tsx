11d3dae553af4cc247e650d256bb23cbb2c9f2aa
duflow89 <duflow89@gmail.com>
2021-11-18 17:25:06 +0900

    test: pre-commit check

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,15 @@
-import reactIcon from '~/resources/images/react-icon.png';
 import svgTest from '~/resources/images/test.svg';
+import reactIcon from '~/resources/images/react-icon.png';
 
-const App = () => {
-  return (
-    <div className='wrap'>
-      <h1>react example</h1>
-      <div className='container'>
-        <img src={svgTest} alt='svg test' width={100} />
-        <img src={reactIcon} alt='react icon' />
-        <div>content</div>
-      </div>
+const App = () => (
+  <div className='wrap'>
+    <h1>react example</h1>
+    <div className='container'>
+      <img src={svgTest} alt='svg test' width={100} />
+      <img src={reactIcon} alt='react icon' />
+      <div>content</div>
     </div>
-  );
-};
+  </div>
+);
 
 export default App;
