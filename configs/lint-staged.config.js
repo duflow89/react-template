84ef188316f1a51fab3ec46e2531e7b48ab0ff1e
duflow89 <duflow89@gmail.com>
2021-11-18 15:38:03 +0900

    feat: add eslint & husky & lint-staged

---
+++ configs/lint-staged.config.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+// https://github.com/okonet/lint-staged/issues/829#issuecomment-631563134
+// eslint-disable-next-line consistent-return
+const generateTSConfig = (stagedFilenames) => {
+  try {
+    const tsconfig = JSON.parse(fs.readFileSync('tsconfig.json', 'utf8'));
+    tsconfig.include = [...stagedFilenames, 'src/global.d.ts'];
+    fs.writeFileSync('tsconfig.lint.json', JSON.stringify(tsconfig));
+
+    return 'tsc --project ./tsconfig.lint.json --noEmit';
+  } catch (e) {
+    console.log('error: ', e);
+  }
+};
+
+module.exports = {
+  '*.{js,jsx}': ['eslint --ext .js,.jsx,.ts,.tsx --fix'],
+  '*.{ts,tsx}': ['eslint --ext .js,.jsx,.ts,.tsx --fix', generateTSConfig],
+};
